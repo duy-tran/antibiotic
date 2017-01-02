f37e585695732df68999bc44f5c22ba89750fabe
Tung Nguyen <tung18tht@gmail.com>
2017-01-02 18:42:21 +0700

    Change medicine ListView to RecyclerView

--- app/src/main/java/vn/edu/usth/antibiotic/AddPrescriptionActivity.java
+++ app/src/main/java/vn/edu/usth/antibiotic/AddPrescriptionActivity.java
@@ -2,11 +2,14 @@ package vn.edu.usth.antibiotic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,23 +20,57 @@ public class AddPrescriptionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_prescription);
     
-        final EditText newMedicineTextbox = (EditText) findViewById(R.id.new_medicine_textbox);
-    
         final ArrayList<String> medicineNames = new ArrayList<>();
-        final ArrayAdapter<String> medicineListAdapter = new ArrayAdapter<>(this, R.layout.medicine_item, R.id.medicine_name, medicineNames);
-    
-        ListView medicineList = (ListView) findViewById(R.id.medicine_list);
+        final MedicineAdapter medicineListAdapter = new MedicineAdapter(medicineNames);
+                
+        RecyclerView medicineList = (RecyclerView) findViewById(R.id.medicine_list);
         medicineList.setAdapter(medicineListAdapter);
+    
+        final EditText newMedicineTextbox = (EditText) findViewById(R.id.new_medicine_textbox);
         
         Button addMedicineButton = (Button) findViewById(R.id.add_medicine_button);
         addMedicineButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String newItem = newMedicineTextbox.getText().toString();
-                medicineNames.add(newItem);
+                String newMedicine = newMedicineTextbox.getText().toString();
+                newMedicineTextbox.setText("");
+                medicineNames.add(newMedicine);
                 medicineListAdapter.notifyDataSetChanged();
-        
             }
         });
+    }
+    
+    class MedicineAdapter extends RecyclerView.Adapter<MedicineAdapter.ViewHolder> {
+        private ArrayList<String> data;
+    
+        class ViewHolder extends RecyclerView.ViewHolder {
+            RelativeLayout medicineView;
+    
+            ViewHolder(RelativeLayout v) {
+                super(v);
+                medicineView = v;
+            }
+        }
+    
+        MedicineAdapter(ArrayList<String> data) {
+            this.data = data;
+        }
+    
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            RelativeLayout v = (RelativeLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.medicine_item, parent, false);
+            return new ViewHolder(v);
+        }
+    
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            TextView medicineName = (TextView) holder.medicineView.findViewById(R.id.medicine_name);
+            medicineName.setText(data.get(position));
+        }
+    
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
     }
 }
