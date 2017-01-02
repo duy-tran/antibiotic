9bc000606a98271ee2ff262157558a44ec16dacb
Tung Nguyen <tung18tht@gmail.com>
2017-01-02 17:45:51 +0700

    Add 'Add prescription' function

--- app/src/main/java/vn/edu/usth/antibiotic/MainActivity.java
+++ app/src/main/java/vn/edu/usth/antibiotic/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 /**
  * Created by duy on 1/2/17.
@@ -24,19 +23,31 @@ public class MainActivity extends AppCompatActivity {
         ((ImageView) findViewById(R.id.alarm_icon)).setColorFilter(red);
         ((ImageView) findViewById(R.id.user_icon)).setColorFilter(white);
         ((ImageView) findViewById(R.id.search_icon)).setColorFilter(white);
-
+    
         findViewById(R.id.search).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openMenu("search");
             }
         });
+    
+        findViewById(R.id.cross).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenu("add");
+            }
+        });
     }
 
     private void openMenu(String name){
         Class cl = null;
-        if (name.equals("search")) {
-            cl = DrugSearchActivity.class;
+        switch (name) {
+            case "search":
+                cl = DrugSearchActivity.class;
+                break;
+            case "add":
+                cl = AddPrescriptionActivity.class;
+                break;
         }
         Intent myIntent = new Intent(getApplicationContext(),cl);
         getApplicationContext().startActivity(myIntent);
