a056344e577e5034a2691cf76b110f76bd71048f
Huong Thao <ththao111@gmail.com>
2017-01-04 15:01:14 +0700

    Add Diagnostic History

--- app/src/main/java/vn/edu/usth/antibiotic/DiagnosticActivity.java
+++ app/src/main/java/vn/edu/usth/antibiotic/DiagnosticActivity.java
@@ -1,9 +1,13 @@
 package vn.edu.usth.antibiotic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by duy on 1/2/17.
@@ -19,5 +23,23 @@ public class DiagnosticActivity extends AppCompatActivity{
         setSupportActionBar(toolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_diagnostic, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.diagnostic_history:
+                Intent intent = new Intent(DiagnosticActivity.this, DiagnosticHistoryActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
