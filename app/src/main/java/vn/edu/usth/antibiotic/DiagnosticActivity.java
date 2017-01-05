6084a6bf8097c62bfff70eb7b3709ef0752cda2d
Tung Nguyen <tung18tht@gmail.com>
2017-01-05 14:19:33 +0700

    Add color filter for diagnostic appbar

--- app/src/main/java/vn/edu/usth/antibiotic/DiagnosticActivity.java
+++ app/src/main/java/vn/edu/usth/antibiotic/DiagnosticActivity.java
@@ -1,13 +1,14 @@
 package vn.edu.usth.antibiotic;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 /**
  * Created by duy on 1/2/17.
@@ -28,6 +29,15 @@ public class DiagnosticActivity extends AppCompatActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_diagnostic, menu);
+    
+        for(int i = 0; i < menu.size(); i++){
+            Drawable drawable = menu.getItem(i).getIcon();
+            if(drawable != null) {
+                drawable.mutate();
+                drawable.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+            }
+        }
+        
         return true;
     }
 
